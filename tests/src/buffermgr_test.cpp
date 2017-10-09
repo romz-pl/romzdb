49442911cb4da2b537ee8e89d26f6d32f62353d3
Zbigniew Romanowski <romz@wp.pl>
2017-10-09 17:12:17 +0200

    Tests for BufferMgr was extended

--- tests/src/buffermgr_test.cpp
+++ tests/src/buffermgr_test.cpp
@@ -23,7 +23,7 @@ TEST(BufferMgr, GetPage)
     // bufferMgr.Print();
 }
 
-TEST(BufferMgr, TooLarge)
+TEST(BufferMgr, TooSmallBuffer)
 {
     UnixFile uf( UnixFile::GetTempPath(), UnixFile::Mode::Create );
     DiskSpaceMgr ds( uf );
@@ -40,3 +40,63 @@ TEST(BufferMgr, TooLarge)
 
     // bufferMgr.Print();
 }
+
+TEST(BufferMgr, WritePage)
+{
+    const PageId pageId = 1;
+    UnixFile uf( UnixFile::GetTempPath(), UnixFile::Mode::Create );
+    DiskSpaceMgr ds( uf );
+    const std::size_t numPages = 3;
+    BufferMgr bufferMgr( ds, numPages );
+
+    EXPECT_ANY_THROW( bufferMgr.MarkDirty( pageId ) );
+    EXPECT_ANY_THROW( bufferMgr.WritePage( pageId ) );
+    EXPECT_ANY_THROW( bufferMgr.UnpinPage( pageId ) );
+
+
+    EXPECT_NO_THROW( bufferMgr.GetPage( pageId, false ) );
+    EXPECT_NO_THROW( bufferMgr.MarkDirty( pageId ) );
+    EXPECT_NO_THROW( bufferMgr.WritePage( pageId ) );
+    EXPECT_NO_THROW( bufferMgr.UnpinPage( pageId ) );
+
+    // bufferMgr.Print();
+}
+
+TEST(BufferMgr, UnpinPage)
+{
+    const PageId pageId = 1;
+    UnixFile uf( UnixFile::GetTempPath(), UnixFile::Mode::Create );
+    DiskSpaceMgr ds( uf );
+    const std::size_t numPages = 3;
+    const std::size_t loopSize = 11;
+    BufferMgr bufferMgr( ds, numPages );
+
+    for( std::size_t i = 0; i < loopSize; i++ )
+        EXPECT_NO_THROW( bufferMgr.GetPage( pageId, true ) );
+
+    for( std::size_t i = 0; i < loopSize; i++ )
+        EXPECT_NO_THROW( bufferMgr.UnpinPage( pageId ) );
+
+    EXPECT_ANY_THROW( bufferMgr.UnpinPage( pageId ) );
+}
+
+TEST(BufferMgr, FlushPages)
+{
+    const PageId pageId = 1;
+    UnixFile uf( UnixFile::GetTempPath(), UnixFile::Mode::Create );
+    DiskSpaceMgr ds( uf );
+    const std::size_t numPages = 3;
+    const std::size_t loopSize = 11;
+    BufferMgr bufferMgr( ds, numPages );
+
+    for( std::size_t i = 0; i < loopSize; i++ )
+        EXPECT_NO_THROW( bufferMgr.GetPage( pageId, true ) );
+
+    EXPECT_ANY_THROW( bufferMgr.FlushPages( ) );
+
+    for( std::size_t i = 0; i < loopSize; i++ )
+        EXPECT_NO_THROW( bufferMgr.UnpinPage( pageId ) );
+
+    EXPECT_NO_THROW( bufferMgr.FlushPages( ) );
+}
+
