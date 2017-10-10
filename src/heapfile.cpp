15b42b91e506910fc7b5b5d8ba7552ecfe70add4
Zbigniew Romanowski <romz@wp.pl>
2017-10-10 09:00:31 +0200

    Interface and description for class HeapFile

--- src/heapfile.cpp
+++ src/heapfile.cpp
@@ -1,1 +1,77 @@
 #include "heapfile.h"
+#include <cassert>
+
+//
+//
+//
+HeapFile::HeapFile( PageId headerPage, Mode mode )
+    : m_headerPage( headerPage )
+{
+    if( mode == Mode::Open )
+    {
+        Open( );
+        return;
+    }
+
+    if( mode == Mode::Create )
+    {
+        Create( );
+        return;
+    }
+
+    assert( 0 );
+}
+
+//
+//
+//
+void HeapFile::Open( )
+{
+
+}
+
+//
+//
+//
+void HeapFile::Create( )
+{
+
+}
+
+//
+//
+//
+Record HeapFile::Get( RecordId rid )
+{
+
+}
+
+//
+//
+//
+void HeapFile::Insert( RecordId rid, const Record& rec )
+{
+
+}
+
+//
+//
+//
+void HeapFile::Delete( RecordId rid )
+{
+
+}
+
+//
+//
+//
+void HeapFile::Update( RecordId rid, const Record& rec )
+{
+    //
+    // This can be done more efficiently for records of the same length.
+    // But deleting "old" record, and inserting "new" record is straithforward.
+    //
+
+    Delete( rid );
+    Insert( rid, rec );
+}
--- src/heapfile.h
+++ src/heapfile.h
@@ -1,10 +1,44 @@
 #ifndef ROMZDB_HEAPFILE_H
 #define ROMZDB_HEAPFILE_H
 
+//
+// 1. This class represents the Heap File, the simplest file structure
+//
+// 2. The data in the pages of Heap File is not ordered in any way.
+//
+// 3. The anly guarantee is, that one can retrieve all records in the file
+//    by repeated requests for the next record.
+//
+// 4. Every record in the file has a unique record ID.
+//
+// 5. Every page in a Heap File is of the same size
+//
+
+#include "pageid.h"
+#include "record.h"
+#include "recordid.h"
+
 class HeapFile
 {
 public:
+public:
+    enum class Mode{ Open, Create };
+public:
+    HeapFile( PageId headerPage, Mode mode );
+    ~HeapFile() = default;
 
+    Record Get( RecordId rid );
+    void Insert( RecordId rid, const Record& rec );
+    void Delete( RecordId rid );
+    void Update( RecordId rid, const Record& rec );
+
+
+private:
+    void Open( );
+    void Create( );
+
+private:
+    const PageId m_headerPage;
 };
 
 #endif
