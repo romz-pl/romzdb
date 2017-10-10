6a466d5ddc86f64c694124533cb5a114b1a3f2bc
Zbigniew Romanowski <romz@wp.pl>
2017-10-10 09:36:35 +0200

    Class HeapFile: more documentation and draft of implementation

--- src/heapfile.cpp
+++ src/heapfile.cpp
@@ -4,8 +4,9 @@
 //
 //
 //
-HeapFile::HeapFile( PageId headerPage, Mode mode )
-    : m_headerPage( headerPage )
+HeapFile::HeapFile( BufferMgr& bufferMgr, PageId headerPage, Mode mode )
+    : m_bufferMgr( bufferMgr )
+    , m_headerPage( headerPage )
 {
     if( mode == Mode::Open )
     {
@@ -41,24 +42,41 @@ void HeapFile::Create( )
 //
 //
 //
-Record HeapFile::Get( RecordId rid )
+Record HeapFile::Get( RecordId /*rid*/ )
 {
+//    HeapPage page( m_bufferMgr.GetPage( rid.GetPageId(), true ) );
+//    const Record rec = page.Get( rid.GetSlotId() );
+//    m_bufferMgr.UnpinPage( rid.GetPageId() );
+//
+//    return rec;
 
+    return Record( {'a'} );
 }
 
 //
 //
 //
-void HeapFile::Insert( RecordId rid, const Record& rec )
+RecordId HeapFile::Insert( const Record& /*rec*/ )
 {
+//    HeapDir heapDir;
+//    const PageId pageId = heapDir.Insert();
+//    HeapPage heapPage( m_bufferMgr.GetPage( pageId, true ) );
+//    const SlotId slotId = heapPage.Insert( rec );
+//    m_bufferMgr.UnpinPage( pageId );
+//
+//    return RecordId( pageId, slotId );
 
+    return RecordId( 0, 0 );
 }
 
 //
 //
 //
-void HeapFile::Delete( RecordId rid )
+void HeapFile::Delete( RecordId /*rid*/ )
 {
+//    HeapPage heapPage( m_bufferMgr.GetPage( rid.GetPageId(), true ) );
+//    heapPage.Delete( rid.GetSlotId() );
+//    m_bufferMgr.UnpinPage( rid.GetPageId() );
 
 }
 
@@ -73,5 +91,5 @@ void HeapFile::Update( RecordId rid, const Record& rec )
     //
 
     Delete( rid );
-    Insert( rid, rec );
+    Insert( rec );
 }
