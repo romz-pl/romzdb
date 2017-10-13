3eff19d843ed1a76d46f1003b40f898d2cf1a5e6
Zbigniew Romanowski <romz@wp.pl>
2017-10-13 09:48:13 +0200

    Update for debugin testing functions

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,20 @@
 #include "buffermgr.h"
 #include "heappage.h"
 #include "record.h"
+#include "dirpage.h"
 #include <cassert>
 
 void Test()
 {
+    Page page;
+    DirPage dpA( page );
+
+    const PageId pageId = 1;
+    dpA.Insert( pageId );
+
+
+    DirPage dpB( page );
+    dpB.Insert( pageId );
 }
 
 
