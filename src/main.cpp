2787a3927810fd160c76677a021d8c07a26e82ad
Zbigniew Romanowski <romz@wp.pl>
2017-10-15 13:40:49 +0200

    Debuging tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
-
+#include "heappage.h"
 
 void Test()
 {
+    UnixFile uf( UnixFile::GetTempPath(), UnixFile::Mode::Create );
+    DiskSpaceMgr ds( uf );
+    const std::size_t frameNo = 3;
+    BufferMgr bufferMgr( ds, frameNo );
 
+
+    const PageId pageId = bufferMgr.GetNew().first;
+    bufferMgr.Unpin( pageId );
+
+    HeapPage hp( bufferMgr, pageId );
+
+    for( int i = 0; i < 10; i++ )
+    {
+        std::string str( "abc:" );
+        str += std::to_string( i );
+        Record rec( str );
+        SlotId slotId = hp.Insert( rec );
+        // The page is NOT empty
+        hp.Get( slotId );
+        Record ret = hp.Get( slotId );
+        // EXPECT_EQ( rec, ret );
+    }
 }
 
 
