1d8f05ab2eb6c6bf5b40df3be7af71a97cfd81c0
Zbigniew Romanowski <romz@wp.pl>
2017-10-15 15:06:45 +0200

    Debuging tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,9 @@
 #include <iostream>
 #include "heappage.h"
-#include "dirpage.h"
+#include "dir.h"
 
 void Test()
 {
-    UnixFile uf( UnixFile::GetTempPath(), UnixFile::Mode::Create );
-    DiskSpaceMgr ds( uf );
-    const std::size_t frameNo = 3;
-    BufferMgr bufferMgr( ds, frameNo );
-
-
-    PageId pageId = bufferMgr.GetNew().first;
-    bufferMgr.Unpin( pageId );
-
-
-    DirPage dp( bufferMgr, pageId );
-
-    const Record rec( "ABC" );
-    ( dp.Insert( rec ).first );
-
-    pageId = bufferMgr.GetNew().first;
-    bufferMgr.Unpin( pageId );
-    dp.InsertPage( pageId );
-
-    ( dp.Insert( rec ).first );
-
-    auto reca = dp.Insert( Record( "a" ) );
-    ( reca.first );
-
-    auto recb = dp.Insert( Record( "b" ) );
-    ( recb.first );
-
-    ( dp.Delete( reca.second ) );
-    ( dp.Delete( recb.second ) );
-
-    ( dp.Delete( recb.second ) );
-    ( dp.Delete( reca.second ) );
 
 }
 
