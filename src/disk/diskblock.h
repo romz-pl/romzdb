2740d2a208b88884d360233c517e0af958dae956
Zbigniew Romanowski <romz@wp.pl>
2018-03-21 07:43:42 +0100

    Disk block reads itself from the file

--- src/disk/diskblock.h
+++ src/disk/diskblock.h
@@ -11,7 +11,7 @@
 #include <array>
 #include <string>
 #include "unixfile.h"
-#include "pageid.h"
+#include "blockid.h"
 #include "diskblock.h"
 
 class DiskBlock
@@ -28,14 +28,14 @@ public:
 
     bool operator== ( const DiskBlock& block ) const;
 
-    void Read( const UnixFile& uf, PageId pageId );
-    void Write( const UnixFile& uf, PageId pageId ) const;
+    void Read( const UnixFile& uf, BlockId blockId );
+    void Write( const UnixFile& uf, BlockId blockId ) const;
 
 private:
     const char* GetData() const;
     char* GetData();
 
-    off_t PageIdToOffset( PageId pageId ) const;
+    off_t PageIdToOffset( BlockId blockId ) const;
 
 private:
     // Data stored on the disk block
