6491d56e05761ce7921fc6daaddb941b0d643312
Zbigniew Romanowski <romz@wp.pl>
2018-03-21 07:42:32 +0100

    Create mapping from logical page ID to pair of Unix file and block ID

--- src/disk/space.cpp
+++ src/disk/space.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include "space.h"
 
 //
@@ -15,8 +16,11 @@ Space::Space( const std::string& path, UnixFile::Mode mode )
 //
 DiskBlock Space::Read( PageId pageId ) const
 {
+    std::pair< const UnixFile*, BlockId > ret = Map( pageId );
+    const UnixFile *uf = ret.first;
+    const BlockId blockId = ret.second;
     DiskBlock block;
-    block.Read( m_uf, pageId );
+    block.Read( *uf, blockId );
     return block;
 }
 
@@ -25,7 +29,10 @@ DiskBlock Space::Read( PageId pageId ) const
 //
 void Space::Write( const DiskBlock &block, PageId pageId ) const
 {
-    block.Write( m_uf, pageId );
+    std::pair< const UnixFile*, BlockId > ret = Map( pageId );
+    const UnixFile *uf = ret.first;
+    const BlockId blockId = ret.second;
+    block.Write( *uf, blockId );
 }
 
 //
@@ -37,5 +44,20 @@ PageId Space::Allocate()
 
     m_uf.Allocate( DiskBlock::Size );
     return PageId( m_nextPage - 1 );
+}
+
+//
+// The function maps "pageId" to the pair (UnixFile, BlockId).
+//
+// The PageId represents the logical page in HeapFile.
+// The Space can be represented by an array of UnixFile.
+// Each UnixFile is the array of disk blocks identyfied by BlockId.
+//
+// Current implementation of Space has one UnixFile only, hence the mapping is trivial.
+//
+std::pair< const UnixFile*, BlockId > Space::Map( PageId pageId ) const
+{
+    assert( pageId.IsValid() );
+    return std::make_pair( &m_uf, BlockId( pageId.GetValue() ) );
 }
 
