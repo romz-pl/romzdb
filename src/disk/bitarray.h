b35fa2290dd0b7f62229d39df2f296cac39a7be8
Zbigniew Romanowski <romz@wp.pl>
2018-03-22 21:40:30 +0100

    Change from template class to normal class

--- src/disk/bitarray.h
+++ src/disk/bitarray.h
@@ -4,12 +4,15 @@
 #include <cstdint>
 #include <climits>
 #include <cassert>
+#include <vector>
 
-template< std::uint32_t SIZE >
 class BitArray
 {
 public:
-    BitArray();
+    BitArray( std::uint32_t bit_no );
+    ~BitArray() = default;
+
+
     void set( std::uint32_t k );
     void reset( std::uint32_t k );
     void flip( std::uint32_t k );
@@ -27,114 +30,15 @@ private:
 
 private:
     // Number of bits in std::uint32_t type
-    static constexpr std::uint32_t m_bits_no = CHAR_BIT * sizeof( std::uint32_t );
+    static constexpr std::uint32_t m_width = CHAR_BIT * sizeof( std::uint32_t );
 
-    std::uint32_t m_array[ ( SIZE + m_bits_no - 1 ) / m_bits_no ];
+    // Number of bits
+    const std::uint32_t m_bit_no;
+
+    std::vector< std::uint32_t > m_array;// [ ( SIZE + m_bits_no - 1 ) / m_bits_no ];
 
 
 };
-
-//
-//
-//
-template< std::uint32_t SIZE >
-BitArray< SIZE >::BitArray()
-    : m_array() // zero initialized
-{
-
-}
-
-//
-//
-//
-template< std::uint32_t SIZE >
-std::uint32_t BitArray< SIZE >::idx( std::uint32_t k ) const
-{
-    assert( k < SIZE );
-    return k / m_bits_no;
-}
-
-//
-//
-//
-template< std::uint32_t SIZE >
-std::uint32_t BitArray< SIZE >::mask( std::uint32_t k ) const
-{
-    assert( k < SIZE );
-    return 1U << ( k % m_bits_no );
-}
-
-//
-// Sets k-th bit to ON
-//
-template< std::uint32_t SIZE >
-void BitArray< SIZE >::set( std::uint32_t k )
-{
-    m_array[ idx( k ) ] |= mask( k );
-}
-
-//
-// Sets k-th bit to OFF
-//
-template< std::uint32_t SIZE >
-void BitArray< SIZE >::reset( std::uint32_t k )
-{
-    m_array[ idx( k ) ] &= ~mask( k );
-}
-
-//
-// Returns "true" if k-th bit is ON
-//
-template< std::uint32_t SIZE >
-bool BitArray< SIZE >::test( std::uint32_t k ) const
-{
-  return ( ( m_array[ idx( k ) ] & mask( k ) ) != 0 );
-}
-
-//
-// Flips k-th bit
-//
-template< std::uint32_t SIZE >
-void BitArray< SIZE >::flip( std::uint32_t k )
-{
-    m_array[ idx( k ) ] ^= mask( k );
-}
-
-//
-//
-//
-template< std::uint32_t SIZE >
-void* BitArray< SIZE >::data()
-{
-    return m_array;
-}
-
-//
-//
-//
-template< std::uint32_t SIZE >
-const void *BitArray<SIZE>::data() const
-{
-    return m_array;
-}
-
-//
-//
-//
-template< std::uint32_t SIZE >
-std::uint32_t BitArray< SIZE >::bit_no() const
-{
-    return SIZE;
-}
-
-//
-//
-//
-template< std::uint32_t SIZE >
-std::uint32_t BitArray< SIZE >::byte_no() const
-{
-    return ( SIZE + CHAR_BIT - 1 ) / CHAR_BIT;
-}
 
 
 #endif
