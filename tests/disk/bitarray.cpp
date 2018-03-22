c0ac2c79e8e336fc6d6cfea18443eceb9044dc96
Zbigniew Romanowski <romz@wp.pl>
2018-03-22 22:28:07 +0100

    More test for BitArray

--- tests/disk/bitarray.cpp
+++ tests/disk/bitarray.cpp
@@ -9,6 +9,9 @@ TEST(BitArray, All)
     const std::uint32_t bits_no = 10000;
     BitArray ba( bits_no );
 
+    EXPECT_TRUE( ba.bit_no() == bits_no );
+    EXPECT_TRUE( ba.data() != nullptr );
+
     for( std::uint32_t k = 0; k < bits_no; k++ )
     {
         EXPECT_FALSE( ba.test( k ) );
