e390bd8d9061f1ce4d73c3ce1b57f1bf6e92dfda
Zbigniew Romanowski <romz@wp.pl>
2018-03-23 19:25:07 +0100

    Create class SpaceMap

---
+++ src/disk/spacemap.h
@@ -0,0 +1,43 @@
+#ifndef ROMZDB_DISK_SPACEMAP_H
+#define ROMZDB_DISK_SPACEMAP_H
+
+#include "unixfile.h"
+#include "blockid.h"
+#include "bitarray.h"
+
+
+class SpaceMap
+{
+public:
+    SpaceMap( UnixFile& uf );
+    SpaceMap( UnixFile& uf, std::uint32_t max_size );
+    ~SpaceMap();
+
+    bool is_allocated( BlockId blockId ) const;
+    BlockId allocate();
+    void free( BlockId blockId );
+
+private:
+    void open( );
+    void create( std::uint32_t max_size );
+    void write();
+
+    std::uint32_t bits_in_header() const;
+    std::uint32_t bits_on_block() const;
+
+private:
+    std::uint32_t m_version;
+
+    std::uint32_t m_map_block_no;
+
+    std::uint32_t m_data_block_no;
+
+    UnixFile& m_uf;
+
+    BitArray *m_bitMap;
+
+
+};
+
+
+#endif
