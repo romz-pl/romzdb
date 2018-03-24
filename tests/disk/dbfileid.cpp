f604a76ad64531035b82e4826f4f72b593ec7666
Zbigniew Romanowski <romz@wp.pl>
2018-03-24 09:37:08 +0100

    Add tests for class DbFieldId

---
+++ tests/disk/dbfileid.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+#include <dbfileid.h>
+
+
+TEST(DbFileId, Comparision)
+{
+    EXPECT_TRUE( DbFileId( 1 ) < DbFileId( 2 ) );
+
+    EXPECT_TRUE( DbFileId( 2 ) > DbFileId( 1 ) );
+
+    EXPECT_TRUE( DbFileId( 2 ) == DbFileId( 2 ) );
+
+    EXPECT_TRUE( DbFileId( 1 ) != DbFileId( 2 ) );
+
+    EXPECT_TRUE( DbFileId( 2 ) <= DbFileId( 2 ) );
+    EXPECT_TRUE( DbFileId( 2 ) <= DbFileId( 3 ) );
+
+    EXPECT_TRUE( DbFileId( 2 ) >= DbFileId( 2 ) );
+    EXPECT_TRUE( DbFileId( 3 ) >= DbFileId( 2 ) );
+}
+
