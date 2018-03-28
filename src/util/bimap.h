56ee6a50889fe2e717cfd1165aef2b4112af1f64
Zbigniew Romanowski <romz@wp.pl>
2018-03-28 17:36:27 +0200

    Refactor bimap class and its tests

--- src/util/bimap.h
+++ src/util/bimap.h
@@ -1,134 +1,170 @@
-#include <unordered_map>
-#include <list>
-#include <assert.h>
+// #include <unordered_map>
+#include <map>
+// #include <list>
+#include <cassert>
 
-template<typename K, typename V>
-class bimap {
-    typedef typename std::unordered_map<K, V>::iterator iter;
-    typedef typename std::unordered_map<K, V>::const_iterator const_iter;
+template< typename K, typename V >
+class bimap
+{
+    //typedef typename std::map< K, V >::iterator iter;
+    // typedef typename std::map< K, V >::const_iterator const_iter;
 
-    std::unordered_map<K, V> key_to_value;
-    std::unordered_map<V, iter> value_to_key;
+    std::map< K, V > m_key_to_value;
+    std::map< V, K > m_value_to_key;
 
 public:
     bimap() = default;
+    ~bimap() = default;
 
-    bimap(bimap const& other) {
-        for (auto key_value_pair : other) { 
-            auto insert_to_key_value_map = key_to_value.insert(key_value_pair);
-            if (!insert_to_key_value_map.second) 
-                throw std::runtime_error("fail in copy ctor");
-            auto insert_to_value_iter_map = value_to_key.emplace(key_value_pair.second, 
-                    insert_to_key_value_map.first);
-            if (!insert_to_value_iter_map.second)
-                throw std::runtime_error("fail in copy ctor");
-        }
-    }
+    bimap( const bimap& ) = default;
+    bimap& operator=( const bimap& ) = default;
 
-    size_t erase_by_key(K const& key) {
-        auto iter = key_to_value.find(key);
-        if (iter == key_to_value.end()) 
-            return 0;
-        // assert(1 == value_to_key.erase(iter->second));
-        // assert(1 == key_to_value.erase(iter->first));
-        value_to_key.erase(iter->second);
-        key_to_value.erase(iter->first);
-        return 1;
-    }
+    bimap( bimap&& ) = default;
+    bimap& operator=( bimap&& ) = default;
 
-    size_t erase_by_value(V const& value) {
-        auto iter = value_to_key.find(value);
-        if (iter == value_to_key.end())
-            return 0;
-        //assert(1 == key_to_value.erase(iter->second->first));
-        //assert(1 == value_to_key.erase(iter->first));
-        key_to_value.erase(iter->second->first);
-        value_to_key.erase(iter->first);
-        return 1;
-    }
-
-    bool insert(K const& key, V const& value) {
-        assert(key_to_value.size() == value_to_key.size());
-        auto insert_to_key_map = key_to_value.emplace(key, value);
-        if (!insert_to_key_map.second) {
+    bool erase_by_key( const K& key)
+    {
+        auto iter = m_key_to_value.find( key );
+        if( iter == m_key_to_value.end() )
+        {
             return false;
         }
-        auto insert_to_value_map = value_to_key.emplace(value, insert_to_key_map.first);
-        if (!insert_to_value_map.second) {
-            key_to_value.erase(insert_to_key_map.first);
-            return false;
-        }
-        assert(key_to_value.size() == value_to_key.size());
+
+        m_key_to_value.erase( iter );
+        m_value_to_key.erase( iter->second );
         return true;
     }
 
-    K const& get_key_by_value(V const& value) {
-        return value_to_key.at(value)->first;
+
+    bool erase_by_value( const V& value )
+    {
+        auto iter = m_value_to_key.find( value );
+        if( iter == m_value_to_key.end() )
+        {
+            return false;
+        }
+
+        m_value_to_key.erase( iter );
+        m_key_to_value.erase( iter->second );
+        return true;
     }
 
-    V const& get_value_by_key(K const& key) {
-        return key_to_value.at(key);
+    bool insert( const K& key, const V& value )
+    {
+        assert( m_key_to_value.size() == m_value_to_key.size() );
+
+        auto insert_to_key_map = m_key_to_value.insert( std::make_pair( key, value ) );
+        if ( !insert_to_key_map.second )
+        {
+            return false;
+        }
+
+        auto insert_to_value_map = m_value_to_key.insert( std::make_pair( value, key ) );
+        if( !insert_to_value_map.second )
+        {
+            m_key_to_value.erase( insert_to_key_map.first );
+            return false;
+        }
+
+        assert( m_key_to_value.size() == m_value_to_key.size() );
+        return true;
     }
 
-    void clear() {
-        key_to_value.clear();
-        value_to_key.clear();
+    K get_key_by_value( const V& value ) const
+    {
+        auto it = m_value_to_key.find( value );
+        if( it == m_value_to_key.end() )
+        {
+            throw std::runtime_error("bimap::get_key_by_value: Cannot find value" );
+        }
+        return it->second;
     }
 
-    bool empty() const noexcept {
-        return key_to_value.empty();
+    V get_value_by_key( const K& key ) const
+    {
+        auto it = m_key_to_value.find( key );
+        if( it == m_key_to_value.end() )
+        {
+            throw std::runtime_error("bimap::get_value_by_key: Cannot find value" );
+        }
+        return it->second;
     }
 
-    size_t size() const noexcept {
-        return key_to_value.size();
+    void clear()
+    {
+        m_key_to_value.clear();
+        m_value_to_key.clear();
     }
 
-    class iterator {
-        const_iter map_iterator;
+    bool empty() const noexcept
+    {
+        assert( m_key_to_value.empty() == m_value_to_key.empty() );
+        return m_key_to_value.empty();
+    }
+
+    size_t size() const noexcept
+    {
+        assert( m_key_to_value.size() == m_value_to_key.size() );
+        return m_key_to_value.size();
+    }
+
+
+    class iterator
+    {
+        typedef typename std::map< K, V >::const_iterator const_iter;
+        const_iter m_it;
 
     public:
         typedef std::input_iterator_tag iterator_category;
 
-        iterator(const_iter map_iterator) : map_iterator{map_iterator} {}
+        iterator( const_iter it ) : m_it{ it }
+        {
+
+        }
         
-        iterator& operator++() {
-            ++map_iterator;
+        iterator& operator++()
+        {
+            ++m_it;
             return *this; 
         }
 
-        iterator operator++(int) {
-            iterator old = this;
-            this++;
+        iterator operator++( int )
+        {
+            iterator old = *this;
+            ++( *this );
             return old;
         }
 
-        typename const_iter::value_type operator*() const {
-            return *map_iterator;
+        typename const_iter::value_type operator*() const
+        {
+            return *m_it;
         }
         
-        typename const_iter::pointer operator->() const {
-            return map_iterator.operator->();
+        typename const_iter::pointer operator->() const
+        {
+            return m_it.operator->();
         }
 
-        bool operator==(iterator const& rhs) {
-            return map_iterator == rhs.map_iterator;            
+        bool operator==( const iterator& rhs ) const
+        {
+            return m_it == rhs.m_it;
         }
 
-        bool operator!=(iterator const& rhs) {
-            return map_iterator != rhs.map_iterator;
-        }
-
-        void swap(iterator& rhs) {
-            swap(map_iterator, rhs.map_iterator);
+        bool operator!=( const iterator& rhs ) const
+        {
+            return m_it != rhs.m_it;
         }
     };
 
-    iterator begin() const noexcept {
-        return iterator(key_to_value.cbegin());
+    iterator begin() const noexcept
+    {
+        return iterator( m_key_to_value.cbegin() );
     }
     
-    iterator end() const noexcept {
-        return iterator(key_to_value.cend());
+    iterator end() const noexcept
+    {
+        return iterator( m_key_to_value.cend() );
     }
+
 };
 
--- tests/util/bimap.cpp
+++ tests/util/bimap.cpp
@@ -1,93 +1,124 @@
 #include "gtest/gtest.h"
 #include "util/bimap.h"
 
-class correctness : public testing::Test
+
+TEST( bimap, empty )
 {
-protected:
-    bimap<int, std::string> map;
+    bimap< int, std::string > map;
+    EXPECT_TRUE( map.size() == 0 );
+    EXPECT_TRUE( map.empty() );
+    EXPECT_NO_THROW( map.clear() );
+}
+
+TEST( bimap, one_element )
+{
+    bimap< int, int > map;
+    map.insert( 1, 2 );
+    EXPECT_EQ( 2, map.get_value_by_key( 1 ) );
+    EXPECT_EQ( 1, map.get_key_by_value( 2 ) );
+    EXPECT_EQ( 1, map.size() );
+    EXPECT_NO_THROW( map.clear() );
+}
+
+
+TEST( bimap, multi_items )
+{
+    bimap< int, std::string > map;
+
+    EXPECT_TRUE( map.insert( 10, "aba"  ) );
+    EXPECT_TRUE( map.insert( 20, "caba" ) );
+    EXPECT_TRUE( map.insert( 30, "xxyy" ) );
+    EXPECT_FALSE( map.empty() );
+    EXPECT_EQ( 3, map.size() );
+
+    EXPECT_EQ( 10, map.get_key_by_value( "aba"  ) );
+    EXPECT_EQ( 20, map.get_key_by_value( "caba" ) );
+    EXPECT_EQ( 30, map.get_key_by_value( "xxyy" ) );
+    EXPECT_FALSE( map.empty() );
+    EXPECT_EQ( 3, map.size() );
+
+    EXPECT_EQ( "aba" , map.get_value_by_key( 10 ) );
+    EXPECT_EQ( "caba", map.get_value_by_key( 20 ) );
+    EXPECT_EQ( "xxyy", map.get_value_by_key( 30 ) );
+    EXPECT_FALSE( map.empty() );
+    EXPECT_EQ( 3, map.size() );
+
+    EXPECT_NO_THROW( map.clear() );
+}
+
+TEST( bimap, insert_element_with_existing_value_or_key )
+{
+    bimap< int, std::string > map;
+
+    EXPECT_TRUE( map.insert( 1, "aba"  ) );
+    EXPECT_TRUE( map.insert( 2, "caba" ) );
+    EXPECT_TRUE( map.insert( 3, "xxyy" ) );
+
+    EXPECT_FALSE( map.insert( 4, "aba" ) );
+    EXPECT_FALSE( map.insert( 3, "QWE" ) );
+    EXPECT_FALSE( map.insert( 2, "caba") );
+
+    EXPECT_EQ( "aba" , map.get_value_by_key( 1 ) );
+    EXPECT_EQ( "caba", map.get_value_by_key( 2 ) );
+    EXPECT_EQ( "xxyy", map.get_value_by_key( 3 ) );
+
+    EXPECT_EQ( 1, map.get_key_by_value("aba"  ) );
+    EXPECT_EQ( 2, map.get_key_by_value("caba" ) );
+    EXPECT_EQ( 3, map.get_key_by_value("xxyy" ) );
+
+    EXPECT_EQ( 3, map.size() );
+}
+
+
+TEST( bimap, geting_absent_value )
+{
+    bimap< int, std::string > map;
+
+    EXPECT_ANY_THROW( map.get_value_by_key( 1 ) );
+    EXPECT_ANY_THROW( map.get_key_by_value( "asdasd" ) );
+}
+
+
+TEST( bimap, delete_existing_element )
+{
+    bimap< int, std::string > map;
+
+    EXPECT_TRUE( map.insert( 10, "aba"  ) );
+    EXPECT_TRUE( map.insert( 20, "caba" ) );
+    EXPECT_TRUE( map.insert( 30, "xxyy" ) );
+
+    EXPECT_TRUE( map.erase_by_key( 20 ) );
+    EXPECT_ANY_THROW( map.get_value_by_key( 20 ) );
+    EXPECT_ANY_THROW( map.get_key_by_value( "caba" ) );
     
-    virtual void SetUp() {
-        EXPECT_TRUE(map.insert(1, "aba"));
-        EXPECT_TRUE(map.insert(2, "caba"));
-        EXPECT_TRUE(map.insert(3, "abacaba")); 
-    }
-};
-
-TEST_F(correctness, empty_map) {
-    bimap<int, std::string> map;
-    EXPECT_EQ(0, map.size());
-    EXPECT_TRUE(map.empty());
-}
-
-TEST_F(correctness, one_element) {
-    bimap<int, int> map;
-    map.insert(1, 2);
-    EXPECT_EQ(2, map.get_value_by_key(1));
-    EXPECT_EQ(1, map.get_key_by_value(2));
-    EXPECT_EQ(1, map.size());
-}
-
-TEST_F(correctness, multi_items) {
-    EXPECT_EQ(1, map.get_key_by_value("aba"));
-    EXPECT_EQ(2, map.get_key_by_value("caba"));
-    EXPECT_EQ(3, map.get_key_by_value("abacaba"));
-    EXPECT_EQ("aba", map.get_value_by_key(1));
-    EXPECT_EQ("caba", map.get_value_by_key(2));
-    EXPECT_EQ("abacaba", map.get_value_by_key(3));
-
-    EXPECT_FALSE(map.empty());
-    EXPECT_EQ(3, map.size());
-}
-
-TEST_F(correctness, insert_element_with_existing_value_or_key) {
-    EXPECT_FALSE(map.insert(4, "aba"));
-    EXPECT_FALSE(map.insert(3, "QWE"));
-    EXPECT_FALSE(map.insert(2, "caba"));
-
-    EXPECT_EQ("aba", map.get_value_by_key(1));
-    EXPECT_EQ("caba", map.get_value_by_key(2));
-    EXPECT_EQ("abacaba", map.get_value_by_key(3));
-    EXPECT_EQ(1, map.get_key_by_value("aba"));
-    EXPECT_EQ(2, map.get_key_by_value("caba"));
-    EXPECT_EQ(3, map.get_key_by_value("abacaba"));
-
-    EXPECT_EQ(3, map.size());
-}
-
-TEST_F(correctness, geting_absent_value) {
-    bimap<int, std::string> map;
-    EXPECT_ANY_THROW(map.get_value_by_key(1));
-    EXPECT_ANY_THROW(map.get_key_by_value("asdasd"));
-}
-
-TEST_F(correctness, delete_existing_element) {
-    EXPECT_TRUE(map.erase_by_key(2));
-    EXPECT_ANY_THROW(map.get_value_by_key(2));
-    EXPECT_ANY_THROW(map.get_key_by_value("caba"));
+    EXPECT_TRUE( map.erase_by_value( "aba" ) );
+    EXPECT_ANY_THROW( map.get_value_by_key( 10 ) );
+    EXPECT_ANY_THROW( map.get_key_by_value( "aba" ) );
     
-    EXPECT_TRUE(map.erase_by_value("aba"));
-    EXPECT_ANY_THROW(map.get_value_by_key(1));
-    EXPECT_ANY_THROW(map.get_key_by_value("aba"));
-    
-    EXPECT_EQ(3, map.get_key_by_value("abacaba"));
-    EXPECT_EQ(1, map.size());
-    EXPECT_FALSE(map.empty());
+    EXPECT_EQ( 30, map.get_key_by_value( "xxyy" ) );
+
+    EXPECT_EQ( 1, map.size() );
+    EXPECT_FALSE( map.empty() );
 }
 
-TEST_F(correctness, delete_absent_element) {
-    bimap<int, std::string> map;
-    map.insert(1, "aba");
-    EXPECT_FALSE(map.erase_by_key(213123));
-    EXPECT_FALSE(map.erase_by_key(131451));
-    EXPECT_FALSE(map.erase_by_value("asdq"));
-    EXPECT_FALSE(map.erase_by_value("asdh"));
+
+TEST( bimap, delete_absent_element )
+{
+    bimap< int, std::string > map;
+    EXPECT_TRUE( map.insert( 1, "aba" ) );
+
+    EXPECT_FALSE( map.erase_by_key( 213123 ) );
+    EXPECT_FALSE( map.erase_by_key( 131451 ) );
+
+    EXPECT_FALSE( map.erase_by_value( "asdq" ) );
+    EXPECT_FALSE( map.erase_by_value( "asdh" ) );
     
-    EXPECT_EQ(1, map.size());
-    EXPECT_FALSE(map.empty());
+    EXPECT_EQ( 1, map.size() );
+    EXPECT_FALSE( map.empty() );
 }
 
+/*
 TEST_F(correctness, random_insert_erase) {
-    /*
     bimap<int, int> map;
     std::set<int> keys_set;
     std::set<int> values_set;
@@ -109,84 +140,99 @@ TEST_F(correctness, random_insert_erase) {
     }
 
     EXPECT_EQ(keys_set.size(), map.size());
-    */
 }
+*/
 
-TEST(copy, bimap_assiginment_copys_independence) {
-    bimap<int, std::string> a; 
-    EXPECT_TRUE(a.insert(1, "abacaba"));
-    EXPECT_TRUE(a.insert(2, "abaa"));
-    EXPECT_TRUE(a.insert(3, "abac")); 
-    bimap<int, std::string> b = a;
+TEST( bimap, copys_independence )
+{
+    bimap< int, std::string > a;
+    EXPECT_TRUE( a.insert( 1, "xxyy" ) );
+    EXPECT_TRUE( a.insert( 2, "abaa" ) );
+    EXPECT_TRUE( a.insert( 3, "abac" ) );
+    bimap< int, std::string > b = a;
 
-    EXPECT_TRUE(a.erase_by_value("abacaba"));
-    EXPECT_TRUE(a.erase_by_key(3));
+    EXPECT_TRUE( a.erase_by_value( "xxyy" ) );
+    EXPECT_TRUE( a.erase_by_key( 3 ) );
 
-    EXPECT_TRUE(b.erase_by_key(1));
-    EXPECT_TRUE(b.erase_by_value("abaa"));
+    EXPECT_TRUE( b.erase_by_key( 1 ) );
+    EXPECT_TRUE( b.erase_by_value( "abaa" ) );
 
-    EXPECT_ANY_THROW(b.get_value_by_key(1));
-    EXPECT_ANY_THROW(b.get_value_by_key(2)); 
-    EXPECT_ANY_THROW(b.get_key_by_value("abacaba"));
-    EXPECT_ANY_THROW(b.get_key_by_value("abaa"));
+    EXPECT_ANY_THROW( b.get_value_by_key( 1 ) );
+    EXPECT_ANY_THROW( b.get_value_by_key( 2 ) );
+    EXPECT_ANY_THROW( b.get_key_by_value( "xxyy" ) );
+    EXPECT_ANY_THROW( b.get_key_by_value( "abaa" ) );
     
-    EXPECT_EQ("abac", b.get_value_by_key(3));
-    EXPECT_EQ(3, b.get_key_by_value("abac"));
-    EXPECT_EQ(1, b.size());
+    EXPECT_EQ( "abac", b.get_value_by_key( 3 ) );
+    EXPECT_EQ( 3, b.get_key_by_value( "abac" ) );
+    EXPECT_EQ( 1, b.size() );
 
-    EXPECT_EQ("abaa", a.get_value_by_key(2));
-    EXPECT_EQ(1, a.size());
+    EXPECT_EQ( "abaa", a.get_value_by_key( 2 ) );
+    EXPECT_EQ( 1, a.size() );
 }
 
-TEST(copy, bimap_copys_independence) {
-    bimap<int, std::string> a; 
-    EXPECT_TRUE(a.insert(1, "abacaba"));
-    EXPECT_TRUE(a.insert(2, "abaa"));
-    EXPECT_TRUE(a.insert(3, "abac")); 
-    bimap<int, std::string> b = a;
 
-    EXPECT_TRUE(a.erase_by_value("abacaba"));
-    EXPECT_TRUE(a.erase_by_key(3));
+TEST( bimap, assiginment_independence )
+{
+    bimap< int, std::string > a;
+    EXPECT_TRUE( a.insert( 1, "xxyy" ) );
+    EXPECT_TRUE( a.insert( 2, "abaa" ) );
+    EXPECT_TRUE( a.insert( 3, "abac" ) );
+    bimap< int, std::string > b;
+    b = a;
 
-    EXPECT_TRUE(b.erase_by_key(1));
-    EXPECT_TRUE(b.erase_by_value("abaa"));
+    EXPECT_TRUE( a.erase_by_value( "xxyy" ) );
+    EXPECT_TRUE( a.erase_by_key( 3 ) );
 
-    EXPECT_ANY_THROW(b.get_value_by_key(1));
-    EXPECT_ANY_THROW(b.get_value_by_key(2)); 
-    EXPECT_ANY_THROW(b.get_key_by_value("abacaba"));
-    EXPECT_ANY_THROW(b.get_key_by_value("abaa"));
+    EXPECT_TRUE( b.erase_by_key( 1 ) );
+    EXPECT_TRUE( b.erase_by_value( "abaa" ) );
+
+    EXPECT_ANY_THROW( b.get_value_by_key( 1 ) );
+    EXPECT_ANY_THROW( b.get_value_by_key( 2 ) );
+    EXPECT_ANY_THROW( b.get_key_by_value( "xxyy" ) );
+    EXPECT_ANY_THROW( b.get_key_by_value( "abaa" ) );
     
-    EXPECT_EQ("abac", b.get_value_by_key(3));
-    EXPECT_EQ(3, b.get_key_by_value("abac"));
-    EXPECT_EQ(1, b.size());
+    EXPECT_EQ( "abac", b.get_value_by_key( 3 ) );
+    EXPECT_EQ( 3, b.get_key_by_value( "abac" ) );
+    EXPECT_EQ( 1, b.size() );
 
-    EXPECT_EQ("abaa", a.get_value_by_key(2));
-    EXPECT_EQ(1, a.size());
+    EXPECT_EQ( "abaa", a.get_value_by_key( 2 ) );
+    EXPECT_EQ( 1, a.size() );
 }
 
-TEST(iterator_correctness, iterate_over_collection) {
-    /*
-    bimap<int, int> map;
-    std::set<int> keys_set;
-    std::set<int> values_set;
-    std::set<std::pair<int, int>> key_value_pairs;
-    for (size_t i = 0; i < 100000; ++i) {
+
+TEST( bimap, iterate_over_collection )
+{
+    bimap< int, int > map;
+    std::set< int > keys_set;
+    std::set< int > values_set;
+    std::set< std::pair< int, int > > key_value_pairs;
+
+    for( size_t i = 0; i < 1000; ++i )
+    {
         int key = std::rand(); 
         int value = std::rand();
-        auto in = map.insert(key, value);
-        EXPECT_TRUE(in == (keys_set.find(key) == keys_set.end()));
-        EXPECT_TRUE(in == (values_set.find(value) == values_set.end()));
-        keys_set.insert(key);
-        values_set.insert(value);
-        key_value_pairs.emplace(key, value);
+        if( map.insert( key, value ) )
+        {
+            keys_set.insert( key );
+            values_set.insert( value );
+            key_value_pairs.insert( std::make_pair( key, value ) );
+        }
     }
 
-    for (auto pair : map) {
-        EXPECT_TRUE(key_value_pairs.find(pair) != key_value_pairs.end());
-        key_value_pairs.erase(pair);
+    for( auto pair : map )
+    {
+        EXPECT_TRUE( key_value_pairs.find( pair ) != key_value_pairs.end() );
     }
 
-    EXPECT_EQ(0, key_value_pairs.size());
-    */
+    for( auto i = map.begin(); i != map.end(); i++ )
+    {
+        EXPECT_TRUE( key_value_pairs.find( *i ) != key_value_pairs.end() );
+        EXPECT_TRUE( map.get_value_by_key( i->first ) );
+        EXPECT_TRUE( map.get_key_by_value( i->second ) );
+
+        key_value_pairs.erase( *i );
+    }
+
+    EXPECT_EQ( 0, key_value_pairs.size() );
 }
 
