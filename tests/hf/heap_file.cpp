8aa7d556f2c870d50518db2e67daf40ed37d22fb
Zbigniew Romanowski <romz@wp.pl>
2018-04-03 21:46:22 +0200

    Extend test for removing records form heap file

--- tests/hf/heap_file.cpp
+++ tests/hf/heap_file.cpp
@@ -1,3 +1,4 @@
+#include <set>
 #include "gtest/gtest.h"
 #include "hf/heap_file.h"
 #include "util/temp_path.h"
@@ -30,5 +31,31 @@ TEST(HeapFile, insert)
     for( int i = 0; i < record_no; i++ )
     {
         EXPECT_NO_THROW( hf.insert( count ) );
+    }
+}
+
+TEST(HeapFile, remove)
+{
+    const uint32_t max_size = ( 1U << 24 );
+    DbFile db_file( ::get_temp_path(), max_size );
+    Space space( db_file );
+    const std::size_t frameNo = 3;
+    BufferMgr bufferMgr( space, frameNo );
+
+    HeapFile hf( bufferMgr );
+
+    std::multiset< PageId > mset;
+
+    const std::uint32_t count = 100;
+    const int record_no = 3000;
+    for( int i = 0; i < record_no; i++ )
+    {
+        const PageId page_id  = hf.insert( count );
+        mset.insert( page_id );
+    }
+
+    for( auto v : mset )
+    {
+        EXPECT_NO_THROW( hf.remove( v, count ) );
     }
 }
