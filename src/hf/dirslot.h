1391055d9a35fbe1f4ad1e28c680e9d4d3cbe4ec
Zbigniew Romanowski <romz@wp.pl>
2018-04-04 17:05:35 +0200

    Make dir slot invalid and empty

--- src/hf/dirslot.h
+++ src/hf/dirslot.h
@@ -8,30 +8,31 @@
 class DirSlot
 {
 public:
-    DirSlot();
-    ~DirSlot() = default;
+    // DirSlot();
+    // ~DirSlot() = default;
 
     bool insert_record( std::uint32_t count );
     bool remove_record( PageId page_id, std::uint32_t count );
 
-    bool add_page( PageId page_id );
-    bool free_page( PageId page_id );
+    bool alloc_page( PageId page_id );
+    bool dispose_page( PageId page_id );
 
-    void make_empty();
+    void make_invalid();
 
     PageId get_page_id() const;
 
-private:
-    bool is_free( std::uint32_t count ) const;
+    bool is_empty() const;
 
 private:
-    bool m_empty;
+    bool m_valid;
 
     // Page into the "DirSlot" points
     PageId m_page_id;
 
     // Free space on the "m_pageId" page
     std::uint32_t m_free_space;
+
+    static const std::uint32_t m_max_free_space;
 };
 
 #endif
