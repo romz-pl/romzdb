087ed9129ca9df2664241768a77783dcf854fe12
Zbigniew Romanowski <romz@wp.pl>
2018-04-07 10:05:33 +0200

    Change test to fixture style

--- tests/hf/heap_file.cpp
+++ tests/hf/heap_file.cpp
@@ -4,17 +4,49 @@
 #include "util/temp_path.h"
 #include "util/random_string.h"
 
-
-TEST(HeapFile, create)
+class HeapFileFixture : public ::testing::Test
 {
-    const uint32_t max_size = ( 1U << 20 );
-    DbFile db_file( ::get_temp_path(), max_size );
-    Space space( db_file );
-    const std::size_t frameNo = 3;
-    BufferMgr bufferMgr( space, frameNo );
+public:
+   HeapFileFixture( )
+       : m_db_file( nullptr )
+       , m_space( nullptr )
+       , m_buffer( nullptr )
+       , m_hf( nullptr )
+   {
+       // initialization code here
+   }
 
-    EXPECT_NO_THROW( HeapFile{ bufferMgr } );
-}
+   void SetUp( )
+   {
+       const uint32_t max_size = ( 1U << 24 );
+       m_db_file = new DbFile( ::get_temp_path(), max_size );
+       m_space = new Space( *m_db_file );
+       const std::size_t frameNo = 3;
+       m_buffer = new BufferMgr( *m_space, frameNo );
+       m_hf = new HeapFile( *m_buffer);
+   }
+
+   void TearDown( )
+   {
+       delete m_hf;
+       delete m_buffer;
+       delete m_space;
+       delete m_db_file;
+   }
+
+   ~HeapFileFixture( )
+   {
+       // cleanup any pending stuff, but no exceptions allowed
+   }
+
+public:
+   // put in any custom data members that you need
+   DbFile *m_db_file;
+   Space *m_space;
+   BufferMgr *m_buffer;
+   HeapFile *m_hf;
+};
+
 
 TEST(HeapFile, open)
 {
@@ -31,40 +63,24 @@ TEST(HeapFile, open)
 }
 
 
-TEST(HeapFile, insert_into_dir)
+TEST_F(HeapFileFixture, insert_into_dir)
 {
-    const uint32_t max_size = ( 1U << 24 );
-    DbFile db_file( ::get_temp_path(), max_size );
-    Space space( db_file );
-    const std::size_t frameNo = 3;
-    BufferMgr bufferMgr( space, frameNo );
-
-    HeapFile hf( bufferMgr );
-
     const std::uint32_t count = 2000;
     const int record_no = 300;
     for( int i = 0; i < record_no; i++ )
     {
-        EXPECT_NO_THROW( hf.insert_into_dir( count ) );
+        EXPECT_NO_THROW( m_hf->insert_into_dir( count ) );
     }
 }
 
-TEST(HeapFile, insert_remove)
+TEST_F(HeapFileFixture, insert_remove)
 {
-    const uint32_t max_size = ( 1U << 24 );
-    DbFile db_file( ::get_temp_path(), max_size );
-    Space space( db_file );
-    const std::size_t frameNo = 3;
-    BufferMgr bufferMgr( space, frameNo );
-
-    HeapFile hf( bufferMgr );
-
     const std::uint32_t count = 2000;
     const int record_no = 3000;
     for( int i = 0; i < record_no; i++ )
     {
         const std::string str = random_string( count );
-        EXPECT_NO_THROW( hf.insert( Record( str ) ) );
+        EXPECT_NO_THROW( m_hf->insert( Record( str ) ) );
     }
 
     std::set< RecordId > sset;
@@ -72,59 +88,43 @@ TEST(HeapFile, insert_remove)
     for( int i = 0; i < record_no; i++ )
     {
         const std::string str = random_string( 200 );
-        RecordId record_id = hf.insert( Record( str ) );
+        RecordId record_id = m_hf->insert( Record( str ) );
         sset.insert( record_id );
     }
 
-    EXPECT_TRUE( hf.get_record_no() == 2 * record_no );
+    EXPECT_TRUE( m_hf->get_record_no() == 2 * record_no );
 
     std::uint32_t rem_no = 0, cnt = 0;
     for( auto v : sset )
     {
         if( cnt % 2 )
         {
-            EXPECT_NO_THROW( hf.remove( v ) );
+            EXPECT_NO_THROW( m_hf->remove( v ) );
             rem_no++;
         }
         cnt++;
     }
 
-    EXPECT_TRUE( hf.get_record_no() == 2 * record_no - rem_no );
+    EXPECT_TRUE( m_hf->get_record_no() == 2 * record_no - rem_no );
 
     for( int i = 0; i < record_no; i++ )
     {
         const std::string str = random_string( 10 );
-        EXPECT_NO_THROW( hf.insert( Record( str ) ) );
+        EXPECT_NO_THROW( m_hf->insert( Record( str ) ) );
     }
 
-    EXPECT_TRUE( hf.get_record_no() == 3 * record_no - rem_no );
+    EXPECT_TRUE( m_hf->get_record_no() == 3 * record_no - rem_no );
 }
 
-TEST(HeapFile, insert_too_long)
+TEST_F(HeapFileFixture, insert_too_long)
 {
-    const uint32_t max_size = ( 1U << 24 );
-    DbFile db_file( ::get_temp_path(), max_size );
-    Space space( db_file );
-    const std::size_t frameNo = 3;
-    BufferMgr bufferMgr( space, frameNo );
-
-    HeapFile hf( bufferMgr );
-
     const std::uint32_t count = 2 * DiskBlock::Size;
     const std::string str( count, 'A' );
-    EXPECT_ANY_THROW( hf.insert( Record( str ) ) );
+    EXPECT_ANY_THROW( m_hf->insert( Record( str ) ) );
 }
 
-TEST(HeapFile, remove_from_dir)
+TEST_F(HeapFileFixture, remove_from_dir)
 {
-    const uint32_t max_size = ( 1U << 24 );
-    DbFile db_file( ::get_temp_path(), max_size );
-    Space space( db_file );
-    const std::size_t frameNo = 3;
-    BufferMgr bufferMgr( space, frameNo );
-
-    HeapFile hf( bufferMgr );
-
     std::multiset< PageId > mset;
     std::set< PageId > sset;
 
@@ -132,48 +132,40 @@ TEST(HeapFile, remove_from_dir)
     const int record_no = 3000;
     for( int i = 0; i < record_no; i++ )
     {
-        const PageId page_id  = hf.insert_into_dir( count );
+        const PageId page_id  = m_hf->insert_into_dir( count );
         mset.insert( page_id );
         sset.insert( page_id );
     }
 
     for( auto v : sset )
     {
-        EXPECT_ANY_THROW( hf.dispose_page( v ) );
+        EXPECT_ANY_THROW( m_hf->dispose_page( v ) );
     }
 
     for( auto v : mset )
     {
-        EXPECT_NO_THROW( hf.remove_from_dir( v, count ) );
+        EXPECT_NO_THROW( m_hf->remove_from_dir( v, count ) );
     }
 
     for( auto v : mset )
     {
-        EXPECT_ANY_THROW( hf.remove_from_dir( v, count ) );
+        EXPECT_ANY_THROW( m_hf->remove_from_dir( v, count ) );
     }
 
     for( auto v : sset )
     {
-        EXPECT_NO_THROW( hf.dispose_page( v ) );
-        EXPECT_ANY_THROW( hf.dispose_page( v ) );
+        EXPECT_NO_THROW( m_hf->dispose_page( v ) );
+        EXPECT_ANY_THROW( m_hf->dispose_page( v ) );
     }
 
     for( auto v : mset )
     {
-        EXPECT_ANY_THROW( hf.remove_from_dir( v, count ) );
+        EXPECT_ANY_THROW( m_hf->remove_from_dir( v, count ) );
     }
 }
 
-TEST(HeapFile, get)
+TEST_F(HeapFileFixture, get)
 {
-    const uint32_t max_size = ( 1U << 24 );
-    DbFile db_file( ::get_temp_path(), max_size );
-    Space space( db_file );
-    const std::size_t frameNo = 3;
-    BufferMgr bufferMgr( space, frameNo );
-
-    HeapFile hf( bufferMgr );
-
     std::map< RecordId, Record > mmap;
 
     const std::uint32_t count = 20;
@@ -182,28 +174,28 @@ TEST(HeapFile, get)
     {
         const std::string str = random_string( count );
         const Record rec( str );
-        const RecordId record_id  = hf.insert( rec );
+        const RecordId record_id  = m_hf->insert( rec );
 
         mmap.insert( std::make_pair( record_id, rec ) );
     }
 
-    EXPECT_TRUE( hf.get_record_no() == record_no );
+    EXPECT_TRUE( m_hf->get_record_no() == record_no );
 
     for( auto v : mmap )
     {
-        EXPECT_TRUE( hf.get( v.first ) == v.second );
+        EXPECT_TRUE( m_hf->get( v.first ) == v.second );
     }
 
 
     for( auto v : mmap )
     {
-        EXPECT_NO_THROW( hf.remove( v.first ) );
+        EXPECT_NO_THROW( m_hf->remove( v.first ) );
     }
 
-    EXPECT_TRUE( hf.get_record_no() == 0 );
+    EXPECT_TRUE( m_hf->get_record_no() == 0 );
 
     for( auto v : mmap )
     {
-        EXPECT_ANY_THROW( hf.get( v.first ) );
+        EXPECT_ANY_THROW( m_hf->get( v.first ) );
     }
 }
